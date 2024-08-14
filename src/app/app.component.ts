0119cd76858110cfafcc965401f05df2cf9ab365
Xolile Dlamini <xolile@lintam.com>
2024-08-14 06:02:13 +0200

    Support tickets app

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [HeaderComponent],
+})
+export class AppComponent {
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
+  currentStatus = 'online';
+}
---
+++ src/app/dashboard/tickeks/tickeks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickeks',
+  standalone: true,
+  imports: [],
+  templateUrl: './tickeks.component.html',
+  styleUrl: './tickeks.component.css'
+})
+export class TickeksComponent {
+
+}
